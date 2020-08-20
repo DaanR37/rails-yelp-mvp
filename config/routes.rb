433240bdca83db02f31bfffcdfb7969b848e964e
Daan Roelofs <daanroelofs599@gmail.com>
2020-08-20 11:26:04 +0200

    Added Rails Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+  resources :reviews, only: [:create]
+  end
 end
+# get 'restaurants', to: 'restaurants#index'
+# get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+# post 'restaurants', to: 'restaurants#create'
+# get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+# get 'restaurants/:id/reviews/new', to: 'review#new', as: :new_review
+# post 'restaurants/:id/reviews', to: 'review#create'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+daan_munchie_place = { name: 'Daan Munchie Place', address: 'Akoleienstraat 48M, 1017LN Amsterdam', phone_number: '00316268535', category: 'belgian' }
+youri_munchie_place = { name: 'Youri Munchie Place', address: 'Vrijheidslaan 50, 1019TJ Amsterdam', phone_number: '00316221435', category: 'italian' }
+bente_munchie_place = { name: 'Bente Munchie Place', address: 'Sarphatistraat 185, 1017WG Amsterdam', phone_number: '00316261065', category: 'chinese' }
+blue_munchie_place = { name: 'Blue Munchie Place', address: 'Latherusstraat 102, 1015AK Amsterdam', phone_number: '00316261825', category: 'japanese' }
+hans_munchie_place = { name: 'Hans Munchie Place', address: 'Overtoom 434, 1017JH Amsterdam', phone_number: '00316273435', category: 'french' }
+
+[daan_munchie_place, youri_munchie_place, bente_munchie_place, blue_munchie_place, hans_munchie_place].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
